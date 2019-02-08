17d0e72faff42dc1ff18797a9e3d7a1288260085
Leon-Arno Rimrod <leonarno.rimrod@gmail.com>
2019-02-08 23:39:58 +0100

    Added inquirer and directory check logic

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-const chalk = require('chalk')
-const clear = require('clear')
-const figlet = require('figlet')
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+
+const files = require('./lib/files');
+const inquirer = require('./lib/inquirer');
+
+clear();
+console.log(
+  chalk.blue(figlet.textSync('Git-Initializer', { horizontalLayout: 'full' }))
+);
+
+if (files.directoryExists('.git')) {
+  console.log(chalk.red('* Already a git repository!'));
+  process.exit();
+}
+
+const run = async () => {
+  const credentials = await inquirer.requestGithubCredentials();
+  console.log(credentials);
+};
