517af22ffa90a55a805cfe0d8f3b13fb6272bbed
Leon-Arno Rimrod <leonarno.rimrod@gmail.com>
2019-02-13 20:38:47 +0100

    refactored runApp functoin to handle authentication and errors

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const ConfigStore = require('configstore');
 const files = require('./lib/files');
 const inquirer = require('./lib/inquirer');
 const githubHandler = require('./lib/githubHandler');
+const repo = require('./lib/repo');
 
 clear();
 console.log(
@@ -17,15 +18,51 @@ if (files.directoryExists('.git')) {
   process.exit();
 }
 
-const getDetails = async () => {
-  let token = github.getStoredGitHubToken();
-  if (!token) {
-    await github.setGitHubCredentials();
-    token = await github.registerNewToken();
+const runApp = async () => {
+  try {
+    const token = await getGitHubToken();
+    github.gitHubAuth(token);
+
+    const url = await repo.createRemoteRepo();
+
+    await repo.createGitIgnore();
+
+    const done = await repo.setupRepository(url);
+    if (done) {
+      console.log(chalk.green('All done!'));
+    }
+  } catch (err) {
+    if (err) {
+      switch (err.code) {
+        case 401:
+          console.log(
+            chalk.red(
+              `Couldn't log you in. Please provide the correct credentials.`
+            )
+          );
+          break;
+        case 422:
+          console.log(chalk.red('Repository name already exists'));
+          break;
+        default:
+          console.log(err);
+      }
+    }
   }
-  console.log(token);
 };
-getDetails();
+runApp();
+
+const getGitHubToken = async () => {
+  let token = github.getStoredGitHubToken();
+  if (token) {
+    return token;
+  }
+
+  await github.setGitHubCredentials();
+
+  token = await github.registerNewToken();
+  return token;
+};
 
 const configure = new ConfigStore('git-initializer');
 
